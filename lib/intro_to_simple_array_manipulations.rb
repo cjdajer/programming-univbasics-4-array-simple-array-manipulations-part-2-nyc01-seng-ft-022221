9599ac8ebf476044f2cc3e92a807ae274aa5bc4b
Christopher Dajer <chrisdajer@gmail.com>
2021-02-05 04:39:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,5 @@
 def using_concat(a, b)
-  a = [1, 2, 3]
-  b = [4, 5, 6]
-  a.concat(b)
+ 
 end 
 
 def using_insert(arrays)
@@ -9,5 +7,5 @@ def using_insert(arrays)
 end 
 
 def using_uniq(arrays, strings, strings)
-  arrays.uniq 
+  
 end 
